aebd8f356f0b58b0c35828368c569e477ac64b16
jo <joeyjot@gmail.com>
2022-03-05 23:53:07 -0800

    animated mascot

---
+++ studybuddy/src/components/Mascot.js
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from "react"; 
+import './Mascot.css';
+
+
+function MascotTail() {
+    return (
+      <div className="Mascot">
+        <div className="Mascot-tail"></div>
+      </div>
+    );
+}
+
+function MascotLookAround() {
+    return (
+        <div className="Mascot">
+            <div className="Mascot-look-around"></div>
+        </div>
+    );
+}
+
+function MascotJumping() {
+    return (
+        <div className="Mascot">
+            <div className="Mascot-jumping"></div>
+        </div>
+    );
+}
+
+function MascotJumpingLook() {
+    return (
+        <div className="Mascot">
+            <div className="Mascot-jumping-look"></div>
+        </div>
+    );
+}
+
+function MascotTailScared() {
+    return (
+        <div className="Mascot">
+            <div className="Mascot-tail-scared"></div>
+        </div>
+    );
+}
+
+function MascotSnooze() {
+    return (
+        <div className="Mascot">
+            <div className="Mascot-snooze"></div>
+        </div>
+    );
+}
+
+function MascotSploot() {
+    return (
+        <div className="Mascot">
+            <div className="Mascot-sploot"></div>
+        </div>
+    );
+}
+
+
+
+export default MascotTail;
+export {MascotLookAround, MascotJumping, MascotJumpingLook, MascotTailScared, MascotSnooze, MascotSploot};
