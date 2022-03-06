bf079fa11e8b7819b53a8c2fe0824ed23aab3a45
aytang-telus <aytang@sfu.ca>
2022-03-06 05:51:30 -0800

    feat: clock on navbar

--- studybuddy/src/components/Navbar.js
+++ studybuddy/src/components/Navbar.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import MenuIcon from "@mui/icons-material/Menu";
-import IconButton from "@mui/material/IconButton";
+import Clock from 'react-live-clock';
 
 export default class Navbar extends Component {
     render() {
         return(
             <AppBar position='static' color='transparent'>
                 <Toolbar>
-                {/* <IconButton>
-                    <MenuIcon />
-                </IconButton> */}
-                <Typography variant="h6" color="inherit" component="div">
+                <Typography variant="h6" color="inherit" component="div"  sx={{ flexGrow: 2 }}>
                     Study Buddy
+                </Typography>
+                <Typography variant="h6" color="inherit" component="div">
+                    <Clock format={'dddd, MMMM Mo, YYYY, h:mm:ss A'} ticking={true} timezone={'US/Pacific'} />
                 </Typography>
                 </Toolbar>
             </AppBar>
